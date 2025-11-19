01689756a783c91abf34a3dee35409058413a6b8
Chris <arthit.pithakchuangchu@gmail.com>
2025-11-19 22:16:50 +0700

    Update screen-todo.tsx

--- todo/app/screen/screen-todo/screen-todo.tsx
+++ todo/app/screen/screen-todo/screen-todo.tsx
@@ -3,7 +3,7 @@ import ContainerTodo from "@/app/containers/container-todo";
 export default function ScreenTodo() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-red-50">Todo App</h1>
+      <h1>Todo App</h1>
       <ContainerTodo />
     </div>
   );
